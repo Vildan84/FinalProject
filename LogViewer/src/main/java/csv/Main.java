3021b720d6e39b7d135e50c81ce89b47fab4ef40
Vildan <vildan84@mail.ru>
2023-01-09 10:36:57 +0300

    added saving chart to file

--- LogViewer/src/main/java/csv/Main.java
+++ LogViewer/src/main/java/csv/Main.java
@@ -4,11 +4,12 @@ import jfreechart.Chart;
 import jfreechart.XYDataset;
 import org.jfree.data.xy.XYSeriesCollection;
 
+import java.io.IOException;
 import java.util.LinkedList;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         ReadCSV r = new ReadCSV();
         CreateColumn col = new CreateColumn();
--- LogViewer/src/main/java/jfreechart/Chart.java
+++ LogViewer/src/main/java/jfreechart/Chart.java
@@ -3,6 +3,7 @@ package jfreechart;
 import csv.Column;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
+import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.block.BlockBorder;
 import org.jfree.chart.plot.PlotOrientation;
@@ -11,11 +12,13 @@ import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYSeriesCollection;
 
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.LinkedList;
 
 public class Chart {
 
-    public void chart(LinkedList<XYSeriesCollection> col, LinkedList<Column> list){
+    public void chart(LinkedList<XYSeriesCollection> col, LinkedList<Column> list) throws IOException {
 
         for(int i = 0; i < col.size(); i++){
             String name = list.get(i + 1).getName();
@@ -24,24 +27,28 @@ public class Chart {
             JFreeChart ch = ChartFactory.createXYLineChart(name, "Engine RPM", value, col.get(i),
                     PlotOrientation.VERTICAL, true, true, false);
 
-            XYPlot plot = ch.getXYPlot();
-            var renderer = new XYLineAndShapeRenderer();
-            renderer.setSeriesPaint(0, Color.BLUE);
-            renderer.setSeriesStroke(0, new BasicStroke(5.0f));
+            File file = new File("Chart" + i);
 
-            plot.setRenderer(renderer);
-            plot.setBackgroundPaint(Color.white);
-            plot.setForegroundAlpha(0.9f);
-            plot.setRangeGridlinePaint(Color.red);
-            plot.setDomainGridlinesVisible(true);
-            plot.setDomainGridlinePaint(Color.black);
-            plot.setDomainGridlinesVisible(true);
+            ChartUtils.saveChartAsPNG(file, ch, 1300, 800);
 
-            ch.getLegend().setFrame(BlockBorder.NONE);
-
-            ChartFrame frame1 = new ChartFrame("lines", ch);
-            frame1.setVisible(true);
-            frame1.setSize(1300, 800);
+//            XYPlot plot = ch.getXYPlot();
+//            var renderer = new XYLineAndShapeRenderer();
+//            renderer.setSeriesPaint(0, Color.BLUE);
+//            renderer.setSeriesStroke(0, new BasicStroke(5.0f));
+//
+//            plot.setRenderer(renderer);
+//            plot.setBackgroundPaint(Color.white);
+//            plot.setForegroundAlpha(0.9f);
+//            plot.setRangeGridlinePaint(Color.red);
+//            plot.setDomainGridlinesVisible(true);
+//            plot.setDomainGridlinePaint(Color.black);
+//            plot.setDomainGridlinesVisible(true);
+//
+//            ch.getLegend().setFrame(BlockBorder.NONE);
+//
+//            ChartFrame frame1 = new ChartFrame("lines", ch);
+//            frame1.setVisible(true);
+//            frame1.setSize(1300, 800);
 
         }
 
